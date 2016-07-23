8de40497a5ce91b3726e27027c96403a719514c2
Rrr <mail@piranis.ru>
2016-07-23 19:02:36 +0400

    animation rc2

--- api.rb
+++ api.rb
@@ -153,10 +153,10 @@ EM.run do
           if @send.ping?
             ping_data = Ping.new(:ip_id => "#{hostname.id}", :rtt => "#{@send.duration}", :timeout => false, :noroute => false)
           else
-            ping_data = Ping.new(:ip_id => "#{hostname.id}", :timeout => true, :noroute => false)
+            ping_data = Ping.new(:ip_id => "#{hostname.id}", :rtt => "", :timeout => true, :noroute => false)
           end
         rescue Errno::EHOSTUNREACH
-          ping_data = Ping.new(:ip_id => "#{hostname.id}", :timeout => false, :noroute => true)
+          ping_data = Ping.new(:ip_id => "#{hostname.id}", :rtt => "", :timeout => false, :noroute => true)
         end
       else
         # Do nothing when hostname disable
