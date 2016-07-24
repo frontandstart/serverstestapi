50e58d16e4ebd8e16ea1ee9f8032dacf4c854c07
Rrr <mail@piranis.ru>
2016-07-24 05:26:06 +0400

    puma fffu

--- puma.rb
+++ puma.rb
@@ -4,3 +4,6 @@ bind "unix:///home/cloud-user/api/tmp/sockets/puma.sock"
 pidfile "/home/cloud-user/api/tmp/pids/puma.pid"
 rackup "/home/cloud-user/api/config.ru"
 state_path "/home/cloud-user/api/tmp/pids/puma.state"
+environment 'production'
+workers 2
+threads 4, 16
