a7e8811ced5b08d970753e7ed96111b9c1973659
Rrr <mail@piranis.ru>
2016-07-28 21:49:59 +0400

    puma setup

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,6 @@ pidfile "#{app_folder}/tmp/pids/puma"
 state_path "#{app_folder}/tmp/state_puma"
 activate_control_app "unix://#{app_folder}/tmp/pids/pumactl.sock"
 rackup "#{app_folder}/config.ru"
-threads 8, 32
-workers 3
-preload_app!
+threads 2, 12
+workers 2
 daemonize true
